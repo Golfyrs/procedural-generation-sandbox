da26c198348de9ca5aea8e94666c3f3eea96aae3
Golfyrs <19377552+Golfyrs@users.noreply.github.com>
2023-10-06 00:38:41 +0300

    Implemented visualization of perlin noise. It's more for debugging purpose.

---
+++ Client/Assets/Examples/1. Perlin noise/Debug/PerlinNoiseVisualization.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Examples._1._Perlin_noise.Debug
+{
+  /// <summary>
+  ///   Visualizes the Perlin noise.
+  /// </summary>
+  public class PerlinNoiseVisualization : MonoBehaviour
+  {
+    [SerializeField] private Renderer renderer;
+
+    [Range(1, 2500)] [SerializeField] private int width = 256;
+    [Range(1, 2500)] [SerializeField] private int height = 256;
+    [SerializeField] private float scale = 20f;
+    [SerializeField] private float offsetX = 100f;
+    [SerializeField] private float offsetY = 100f;
+
+    [SerializeField] public bool AutoUpdate = true;
+
+    public void Visualize()
+    {
+      var texture = new Texture2D(width, height);
+
+      var colors = new Color[width * height];
+
+      for (var x = 0; x < width; x++)
+      for (var y = 0; y < height; y++)
+      {
+        var xCoord = (float) x / width * scale + offsetX;
+        var yCoord = (float) y / height * scale + offsetY;
+        var sample = Mathf.PerlinNoise(xCoord, yCoord);
+        colors[y * width + x] = new(sample, sample, sample);
+      }
+
+      texture.SetPixels(colors);
+      texture.Apply();
+
+      renderer.material.mainTexture = texture;
+    }
+  }
+}
---
+++ Client/Assets/Examples/1. Perlin noise/Debug/PerlinNoiseVisualizationEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Examples._1._Perlin_noise.Debug
+{
+  /// <summary>
+  ///   Custom editor for the PerlinNoiseVisualization component.
+  /// </summary>
+  [CustomEditor(typeof(PerlinNoiseVisualization))]
+  public class PerlinNoiseVisualizationEditor : Editor
+  {
+    public override void OnInspectorGUI()
+    {
+      var visualization = (PerlinNoiseVisualization) target;
+
+      // Button to refresh the visualization.
+      if (GUILayout.Button("Refresh"))
+        visualization.Visualize();
+
+      // Draw the default inspector.
+      if (DrawDefaultInspector())
+        if (visualization.AutoUpdate)
+          visualization.Visualize();
+    }
+  }
+}
